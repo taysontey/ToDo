232d529165a39cb8e424e5cb2baf2e3b984e2322
Tayson de Souza Oliveira <taysontey@gmail.com>
2018-08-21 20:14:03 -0300

    Classes de dominio adicionadas

---
+++ ToDo.Dominio/Entidades/SubTarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.Dominio.Entidades
+{
+    public class SubTarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
---
+++ ToDo.Dominio/Entidades/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ToDo.Dominio.Entidades
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+
+        public IEnumerable<SubTarefa> SubTarefas { get; set; }
+    }
+}
